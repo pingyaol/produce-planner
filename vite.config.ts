459fd7629fd8c24c879588beaff5971f44608d35
pingyaol <pingyao.lpy@gmail.com>
2024-05-06 00:25:45 -0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path'; 
+
+export default defineConfig({
+  root: './src',
+  build: {
+    outDir: '../dist',
+    minify: false,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'), 
+        nested: resolve(__dirname, 'src/product.html'),
+      },
+    }
+  },
+});
+
